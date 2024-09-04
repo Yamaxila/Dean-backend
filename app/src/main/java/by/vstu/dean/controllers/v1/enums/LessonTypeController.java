2e62af8d937fc3aac75646c86da7ca246fcb705c
yamaxila <ronl.yamaxila@gmail.com>
2024-09-04 13:03:43 +0300

    Исправлен пакет by.vstu.controllers.

---
+++ app/src/main/java/by/vstu/dean/controllers/v1/enums/LessonTypeController.java
@@ -0,0 +1,15 @@
+package by.vstu.dean.controllers.v1.enums;
+
+import by.vstu.dean.core.controllers.EnumController;
+import by.vstu.dean.enums.ELessonType;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/v1/enums/lessons/")
+@Tag(name = "ELessonType", description = "Тип занятия")
+public class LessonTypeController extends EnumController<ELessonType> {
+    public LessonTypeController() {
+        this.eEnum = ELessonType.UNKNOWN;
+    }
+}
---
+++ app/src/main/java/by/vstu/dean/controllers/v1/students/InstitutionController.java
@@ -0,0 +1,30 @@
+package by.vstu.dean.controllers.v1.students;
+
+import by.vstu.dean.core.controllers.BaseController;
+import by.vstu.dean.dto.v1.students.InstitutionDTO;
+import by.vstu.dean.mapper.v1.InstitutionMapper;
+import by.vstu.dean.models.students.InstitutionModel;
+import by.vstu.dean.repo.InstitutionModelRepository;
+import by.vstu.dean.services.InstitutionService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Контроллер для работы с учреждениями образования студентов.
+ */
+@RestController
+@RequestMapping("/api/v1/institutions/")
+@Tag(name = "Institutions", description = "Учреждения образования")
+public class InstitutionController extends BaseController<InstitutionDTO, InstitutionModel, InstitutionMapper, InstitutionModelRepository, InstitutionService> {
+
+    /**
+     * Конструктор контроллера.
+     *
+     * @param service Сервис учреждений образования студентов
+     * @param mapper Маппер
+     */
+    public InstitutionController(InstitutionService service, InstitutionMapper mapper) {
+        super(service, mapper);
+    }
+}
