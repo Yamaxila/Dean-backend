9e7f01e4ca665b67fcde3f83f94879e3f0002d9f
yamaxila <ronl.yamaxila@gmail.com>
2024-11-08 19:39:35 +0300

    Тест создания модуля, отличного от деканата. Расписание
    Signed-off-by: yamaxila <ronl.yamaxila@gmail.com>

---
+++ timetable-module/src/main/java/by/vstu/dean/timetable/models/LessonModel.java
@@ -0,0 +1,44 @@
+package by.vstu.dean.timetable.models;
+
+import by.vstu.dean.core.models.DBBaseModel;
+import by.vstu.dean.models.lessons.DisciplineModel;
+import by.vstu.dean.models.lessons.TeacherModel;
+import by.vstu.dean.models.rooms.ClassroomModel;
+import by.vstu.dean.models.students.GroupModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "timetable_lessons")
+@Getter
+@Setter
+public class LessonModel extends DBBaseModel {
+
+    @JoinColumn(name = "group_id")
+    @ManyToOne
+    private GroupModel group;
+    @JoinColumn(name = "teacher_id")
+    @ManyToOne
+    private TeacherModel teacher;
+    @JoinColumn(name = "discipline_id")
+    @ManyToOne
+    private DisciplineModel discipline;
+    @JoinColumn(name = "room_id")
+    @ManyToOne
+    private ClassroomModel room;
+    private Short lessonNumber;
+    private Short day;
+    private Integer lessonType;
+    private Integer weekType;
+    private Integer subGroup;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private boolean visible;
+
+}
---
+++ timetable-module/src/main/java/by/vstu/dean/timetable/repo/LessonModelRepository.java
@@ -0,0 +1,9 @@
+package by.vstu.dean.timetable.repo;
+
+import by.vstu.dean.core.repo.DBBaseModelRepository;
+import by.vstu.dean.timetable.models.LessonModel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonModelRepository extends DBBaseModelRepository<LessonModel> {
+}
