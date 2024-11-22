cf19c7cdc9dfbabf8977d0bb3ae38d218a42cf77
Kr0lb <pivamaks@gmail.com>
2024-11-22 11:41:37 +0300

    Добавлена сортировка отработок

--- student-account/src/main/java/by/vstu/dean/students/services/StudentAbsenceService.java
+++ student-account/src/main/java/by/vstu/dean/students/services/StudentAbsenceService.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,7 +24,8 @@ public class StudentAbsenceService {
     public List<StudentAbsenceMonthDTO> findAbsencesByStudent() {
         Map<LocalDate, List<StudentAbsenceDTO>> map = this.v1StudentAbsenceMapperImpl
                 .toDto(this.absenceService.getAbsenceByStudentCaseNo(Long.parseLong(studentGradeService.jwtCustomTokenDecoder("id_from_source"))))
-                .stream().collect(Collectors.groupingBy(s -> s.getDate().withDayOfMonth(1), TreeMap::new, Collectors.toList()));
+                .stream().sorted(Comparator.comparing(StudentAbsenceDTO::getDate).reversed())
+                .collect(Collectors.groupingBy(s -> s.getDate().withDayOfMonth(1), TreeMap::new, Collectors.toList()));
 
         List<StudentAbsenceMonthDTO> studentAbsenceMonthDTOS = new ArrayList<>();
 
